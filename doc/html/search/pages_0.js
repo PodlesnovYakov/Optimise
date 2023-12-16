ba26e715afdcc68d9121e09e289a337574626cae
Yakov <yakov.podlesnov>
2023-12-16 11:59:56 +0300

    Add virtual destructors and change language

---
+++ doc/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['optimise_0',['Optimise',['../md__r_e_a_d_m_e.html',1,'']]]
+];
--- doc/html/search/searchdata.js
+++ doc/html/search/searchdata.js
@@ -3,7 +3,8 @@ var indexSectionsWithContent =
   0: "acdefgiklmnoprsuv",
   1: "adfgprsv",
   2: "cgimno",
-  3: "acefgklpu"
+  3: "acefgklpu",
+  4: "o"
 };
 
 var indexSectionNames =
@@ -11,7 +12,8 @@ var indexSectionNames =
   0: "all",
   1: "classes",
   2: "functions",
-  3: "variables"
+  3: "variables",
+  4: "pages"
 };
 
 var indexSectionLabels =
@@ -19,6 +21,7 @@ var indexSectionLabels =
   0: "All",
   1: "Classes",
   2: "Functions",
-  3: "Variables"
+  3: "Variables",
+  4: "Pages"
 };
 
