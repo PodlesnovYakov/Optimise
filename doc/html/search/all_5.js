092cad3908d08de34a93f63d429f594562bb642c
Yakov <yakov.podlesnov>
2023-12-20 23:50:10 +0300

    Change documentation

--- doc/html/search/all_5.js
+++ doc/html/search/all_5.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['getvalf_0',['getvalf',['../class_abstr_func.html#aa373bf9bc53d6b1681ac67198728f53e',1,'AbstrFunc::GetValF()'],['../class_f2.html#a5230a15eda16d6f6b6b0250f70b8b843',1,'F2::GetValF()'],['../class_f3.html#af688d68c1049d6f00be9b24a705296cc',1,'F3::GetValF()'],['../class_f4.html#a64504b4a88c9ab65a4506d0af28cb45f',1,'F4::GetValF()']]],
-  ['goldenselection_1',['GoldenSelection',['../class_det_opt.html#a23920cdcf918151d374775723062b3d8',1,'DetOpt']]],
-  ['grad_2',['grad',['../class_abstr_stop.html#a5c185040e8cec18578ab1f18049773a5',1,'AbstrStop']]],
-  ['gradstop_3',['GradStop',['../class_grad_stop.html',1,'']]]
+  ['getdimension_0',['getdimension',['../class_abstr_func.html#ad5691b49f2de9c2983e9f67150af54d7',1,'AbstrFunc::getDimension()'],['../class_f2.html#ad69a308dae98270c2078916c4766f691',1,'F2::getDimension()'],['../class_f3.html#a0e7de433da82c86e4db19e2857d2fca3',1,'F3::getDimension()'],['../class_f4.html#a5075866049065817b50172266daf6353',1,'F4::getDimension()']]],
+  ['getvalf_1',['getvalf',['../class_abstr_func.html#aa373bf9bc53d6b1681ac67198728f53e',1,'AbstrFunc::GetValF()'],['../class_f2.html#a5230a15eda16d6f6b6b0250f70b8b843',1,'F2::GetValF()'],['../class_f3.html#af688d68c1049d6f00be9b24a705296cc',1,'F3::GetValF()'],['../class_f4.html#a64504b4a88c9ab65a4506d0af28cb45f',1,'F4::GetValF()']]],
+  ['goldenselection_2',['GoldenSelection',['../class_det_opt.html#a23920cdcf918151d374775723062b3d8',1,'DetOpt']]],
+  ['grad_3',['grad',['../class_abstr_stop.html#a5c185040e8cec18578ab1f18049773a5',1,'AbstrStop']]],
+  ['gradstop_4',['GradStop',['../class_grad_stop.html',1,'']]]
 ];
